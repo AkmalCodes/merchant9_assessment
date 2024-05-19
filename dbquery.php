dda134af8929f98719e3867abd64b163a612551f
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-05-19 10:53:35 +0800

    encapsulated code into a function

--- dbquery.php
+++ dbquery.php
@@ -1,5 +1,31 @@
-<?php 
-    include 'dbconnect.php';
-    $stmt = mysqli_stmt_init($con);
+<?php
+include 'dbconnect.php';
 
-    $query = "SELECT client_id, client_secret FROM "
+function getClientData()
+{   
+    $con = dbConnect(); // function defined in dbconnect.php
+    $stmt = mysqli_stmt_init($con); // open the connection
+    $query = "SELECT id, secret FROM oauth_clients";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $client_id, $client_secret);
+
+        $results = [];
+        while (mysqli_stmt_fetch($stmt)) {
+            $results[] = [
+                'client_id' => $client_id,
+                'client_secret' => $client_secret,
+            ];
+        }
+        mysqli_stmt_close($stmt); // close the statement
+    }
+    mysqli_close($con); //close the connection
+
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+
+if(isset($_GET['action'])){
+    getClientData();
+}
